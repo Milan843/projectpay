7f1e8c0bcf4d0ed7002ef5a9e9022386c8c5a235
Param4895 <paramjeet.kaur@mail.vinove.com>
2020-01-14 16:09:18 +0530

    RefinedStructure and Service added

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,11 @@ var path = require('path');
 
 module.exports = function (app) {
 
+    const swaggerUi = require('swagger-ui-express');
+    const swaggerDocument = require('./../swagger.json');
+
+    app.use('/api', swaggerUi.serve);
+    app.get('/api', swaggerUi.setup(swaggerDocument));
     // Insert routes below
     app.use('/api/user', require('./api/user'));
     app.use('/api/distributor', require('./api/distributor'));
